77fa3bfbeafad9c7c7f9a009b1a8e63e3aae6121
Mohan7126j <mohan7126j@gmail.com>
2024-06-13 02:41:15 +0000

    Date after 100 Days from Today

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const {addDays, format} = require('date-fns')
+
+const app = express()
+
+let dateTime = new Date();
+let result  = format(addDays(dateTime, 100), "dd-MM-yyyy");
+
+app.get('/', (request, response) =>{
+    response.send(result)
+})
+
+app.listen(3000)
+
+module.exports = app
